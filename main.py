66342313e658ebd7cc03de55e7e8333298f3fae5
Gabriel Coquillet <gabriel.coquillet@gmail.com>
2024-05-08 18:42:50 +0200

    added main structure, begin working on cell detect detection.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('hello world')
+import pyxel as pyx
+
+
+height = 1080
+width = 1920
+cells = [[15,15],[30,30],[45,45]]
+color = 7
+
+pyx.init(height=height,width=width,title="Game Of Life")
+
+def prox(x_cell, y_cell):
+    if pyx.pget(x_cell-2, y_cell+2) == color:
+        return True
+    elif pyx.pget(x_cell+2, y_cell-2) == color:
+        return True    
+    elif pyx.pget(x_cell+6, y_cell+2) == color:
+        return True
+    elif pyx.pget(x_cell+6, y_cell+6) == color:
+        return True
+
+def update():
+    for cell in cells:
+        if prox(cell[0], cell[1]) == False:
+            cells.remove(cell)
+
+def draw():
+    for cell in cells:
+        pyx.rect(cell[0], cell[1], 4,4, color)
+
+pyx.run(update, draw)
