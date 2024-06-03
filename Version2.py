f0d6ae54e392a017e84c9cbbbc16bbbdbee4ab79
Gabriel Coquillet <163413154+GabrielCoquillet@users.noreply.github.com>
2024-06-03 14:45:55 +0200

    Update Version2.py

--- Version2.py
+++ Version2.py
@@ -1,19 +1,11 @@
-import pyxel as pyx
+import pyxel
 
-
-size = 128
-alive_cells = [[15,15],[16,16],[16,17],[15,17],[14,17]]
-dead_cells = [[i,j] for i in range(128) for j in range(128)]
-print(dead_cells[:10])
-for cell in alive_cells:
-    dead_cells.remove(cell)
-color = 7
-cpt_t = 0
+alive_cells = []
+dead_cells = [[i,j] for i in range(0,128) for j in range(0,128) if [i,j] not in alive_cells]
 cam_x , cam_y = 0, 0
 jeu_lancé = False
 
-
-pyx.init(height=size,width=size,title="Game Of Life", fps=5)
+pyxel.init(height=128,width=128,title="Game Of Life", fps=10)
 
 def prox(x_cell, y_cell):
     cpt = 0
@@ -35,25 +27,42 @@ def prox(x_cell, y_cell):
         cpt+=1
     return cpt
 
+def change_stat(alive_cells, dead_cells):
+    to_dead = []
+    to_alive = []
+    for cell in alive_cells:
+        if prox(cell[0], cell[1])!=2 and prox(cell[0], cell[1])!=3:
+            to_dead.append(cell)
+    for cell in dead_cells:
+        if prox(cell[0], cell[1])==3:
+            to_alive.append(cell)
+    for cell in to_alive:
+        dead_cells.remove(cell)
+        alive_cells.append(cell)
+    for cell in to_dead:
+        alive_cells.remove(cell)
+        dead_cells.append(cell)
+    return alive_cells, dead_cells
+
 def selectionner() :
-    if pyx.btnr(pyx.MOUSE_BUTTON_LEFT) :
-        if not [(cam_x+pyx.mouse_x)//2,(cam_y+pyx.mouse_y)//2] in alive_cells :
-            alive_cells.append([(cam_x+pyx.mouse_x)//2,(cam_y+pyx.mouse_y)//2])
-            dead_cells.remove([(cam_x+pyx.mouse_x)//2,(cam_y+pyx.mouse_y)//2])
+    if pyxel.btnr(pyxel.MOUSE_BUTTON_LEFT) :
+        if not [(cam_x+pyxel.mouse_x)//2,(cam_y+pyxel.mouse_y)//2] in alive_cells :
+            alive_cells.append([(cam_x+pyxel.mouse_x)//2,(cam_y+pyxel.mouse_y)//2])
+            dead_cells.remove([(cam_x+pyxel.mouse_x)//2,(cam_y+pyxel.mouse_y)//2])
         else :
-            dead_cells.append([(cam_x+pyx.mouse_x)//2,(cam_y+pyx.mouse_y)//2])
-            alive_cells.remove([(cam_x+pyx.mouse_x)//2,(cam_y+pyx.mouse_y)//2])
+            dead_cells.append([(cam_x+pyxel.mouse_x)//2,(cam_y+pyxel.mouse_y)//2])
+            alive_cells.remove([(cam_x+pyxel.mouse_x)//2,(cam_y+pyxel.mouse_y)//2])
 
 def cam_change(x, y):
-    if pyx.btnr(pyx.KEY_UP):
-        y-=10
-    elif pyx.btnr(pyx.KEY_DOWN):
-        y+=10
-    elif pyx.btnr(pyx.KEY_LEFT):
-        x-=10
-    elif pyx.btnr(pyx.KEY_RIGHT):
-        x+=10
-    pyx.camera(x, y)
+    if pyxel.btnr(pyxel.KEY_UP):
+        y-=5
+    elif pyxel.btnr(pyxel.KEY_DOWN):
+        y+=5
+    elif pyxel.btnr(pyxel.KEY_LEFT):
+        x-=5
+    elif pyxel.btnr(pyxel.KEY_RIGHT):
+        x+=5
+    pyxel.camera(x, y)
     return x, y
 
 def update():
@@ -61,39 +70,25 @@ def update():
     global cam_x, cam_y, jeu_lancé, alive_cells, dead_cells
 
     if jeu_lancé == True :
-        to_dead = []
-        to_alive = []
-        for cell in alive_cells:
-            if prox(cell[0], cell[1])!=2 and prox(cell[0], cell[1])!=3:
-                to_dead.append(cell)
-        for cell in dead_cells:
-            if prox(cell[0], cell[1])==3:
-                to_alive.append(cell)
-        for cell in to_alive:
-            dead_cells.remove(cell)
-            alive_cells.append(cell)
-        for cell in to_dead:
-            alive_cells.remove(cell)
-            dead_cells.append(cell)
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            jeu_lancé = False
+        alive_cells, dead_cells = change_stat(alive_cells, dead_cells)
         cam_x, cam_y = cam_change(cam_x, cam_y)
 
-        if pyx.btnr(pyx.KEY_RETURN) :
+        if pyxel.btnr(pyxel.KEY_RETURN) :
             dead_cells=[[i,j] for i in range(128) for j in range(128)]
             alive_cells=[]
             jeu_lancé=False
 
     elif jeu_lancé==False :
         selectionner()
-        if pyx.btnr(pyx.KEY_SPACE) :
+        if pyxel.btnr(pyxel.KEY_SPACE) :
             jeu_lancé=True
 
 def draw():
-    pyx.cls(0)
-    #borders
-    pyx.rect(-2,-2,260,260,13)
-    pyx.rect(0,0,256,256,0)
-    pyx.rect(pyx.mouse_x, pyx.mouse_y, 2,2,7)
+    pyxel.cls(0)
+    pyxel.rect(pyxel.mouse_x, pyxel.mouse_y, 2,2,7)
     for cell in alive_cells:
-        pyx.rect(cell[0]*2, cell[1]*2, 2,2, color)
+        pyxel.rect(cell[0]*2, cell[1]*2, 2,2, 7)
 
-pyx.run(update, draw)
+pyxel.run(update, draw)
